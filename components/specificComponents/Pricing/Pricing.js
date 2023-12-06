365428ebc53e5fac06c0bd13c98c253b93d3bae6
Mitchelladerijk <146757074+Mitchelladerijk@users.noreply.github.com>
2023-12-06 11:49:24 +0100

    Location verwijderd

---
+++ components/specificComponents/Pricing/Pricing.js
@@ -0,0 +1,15 @@
+import React, { component } from "react";
+
+export default class Pricing extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+            Location:{this.props.blok.title}</div>
+13. );
+14. }
+15. }
+
